d875505e6413ad0b4589924a8ead596fe6a202f9
Caio Ferrari <caioferrari0484@gmail.com>
2021-05-28 10:04:38 -0300

    Bugfixes;

--- 1Bim/S03/tarefa_s03_01.java
+++ 1Bim/S03/tarefa_s03_01.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class tarefa_s03_01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int cont = 0, depositoMax = 0,depositoMin = 1;
+        int cont = 0;
+        float depositoMax = 0,depositoMin = 1;
         
         System.out.println("Insira o contador: ");
         cont = scan.nextInt();
 
-        int[] notas = new int[cont];
+        float[] notas = new float[cont];
 
         for (int i = 0; i<cont;i++) {
             System.out.println("Insira um número: ");
             notas[i] = scan.nextInt();
         }
         depositoMin = notas[0];
-        for (int i = 0; i<cont-1;i++) {
+        for (int i = 0; i<cont;i++) {
             if (notas[i] > depositoMax) {
                 depositoMax = notas[i];
             }
