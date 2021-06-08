772595c434997b9d645349af74d6ca6ba8b0145d
Caio Ferrari <caioferrari0484@gmail.com>
2021-06-08 12:34:14 -0300

    Initial commit;

---
+++ 1Bim/S05/main.java
@@ -0,0 +1,51 @@
+class Main {
+    public static void main(String[] args) {
+        int[] numeros = {1, 4, 5, 7, 9, 11, 15};
+        int numero = 15;
+        String[] nomes = {"Caio","Mané","Moné","Muné"};
+        String nome = "Muné";
+        int posicaoEncontrada = buscaBinaria(numero, numeros);
+        int posicaoNome = buscaNomes(nome, nomes);
+        System.out.println(posicaoEncontrada);
+        System.out.println(posicaoNome);
+      }
+    
+      public static int buscaBinaria(int elemento, int[] elementos) {
+        int inicio = 0;
+        int fim = elementos.length - 1;
+        int meio;
+    
+        while(inicio <= fim) {
+          meio = (int) (inicio + fim) / 2;
+    
+          if(elemento == elementos[meio]) {
+            return meio;
+          } else if(elemento > elementos[meio]) {
+            inicio = meio + 1;
+          } else {
+            fim = meio -1;
+          }
+        }
+    
+        return -1;
+      }
+      public static int buscaNomes(String nome, String[] nomes) {
+        int inicio = 0;
+        int fim = nomes.length - 1;
+        int meio;
+
+        while(inicio <= fim) {
+          meio = (int) (inicio + fim) / 2;
+    
+          if(nome.compareTo(nomes[meio]) == 0) {
+            return meio;
+          } else if(nome.compareTo(nomes[meio]) > 0) {
+            inicio = meio + 1;
+          } else {
+            fim = meio -1;
+          }
+        }
+        
+        return -1;
+      }
+}
