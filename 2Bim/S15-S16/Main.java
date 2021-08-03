d112a1cdd0facfce2345a7e5c11e72d7e3dd53d6
Caio Ferrari <caioferrari0484@gmail.com>
2021-08-03 17:18:08 -0300

    Update

--- 2Bim/S15-S16/Main.java
+++ 2Bim/S15-S16/Main.java
@@ -54,5 +54,51 @@ class Main {
         retanguloTres.calcularArea();
         retanguloTres.calcularPerimetro();
         System.out.println(retanguloTres.getResultados());
+
+        Conta c1 = new Conta(1,0.0,50);
+
+        Conta c2 = new Conta(2,100.00,100);
+
+        c1.depositar(120.00);
+        c2.depositar(50.00);
+        
+        //Irá dar errado, ultrapassou o limite
+        if(c1.sacar(110.00)) System.out.println("Operação realizada com sucesso!"); else System.out.println("ERRO!");
+
+        //Irá dar errado, ultrapassou o limite
+        if(c2.sacar(200.00)) System.out.println("Operação realizada com sucesso!"); else System.out.println("ERRO!"); 
+
+        //Irá dar certo, não ultrapassou o limite
+        if(c2.sacar(100.00)) System.out.println("Operação realizada com sucesso!"); else System.out.println("ERRO!");
+        
+        //Irá dar errado, ultrapassou o saldo
+        if(c2.sacar(100.00)) System.out.println("Operação realizada com sucesso!"); else System.out.println("ERRO!");
+        
+        //Irá dar certo, não ultrapassou o limite    
+        if(c1.sacar(50.00)) System.out.println("Operação realizada com sucesso!"); else System.out.println("ERRO!");
+
+        //Irá dar errado, ultrapassou o saldo
+        if(c1.sacar(90.00)) System.out.println("Operação realizada com sucesso!"); else System.out.println("ERRO!");
+
+        /*~~~~~~Tentando usar ternárias, sucesso!~~~~~~*/
+
+        //Irá dar certo, não ultrapassou nenhum limitante
+        System.out.println((c2.transferir(c1, 20.00) ? "Operação realizada com sucesso!" : "ERRO!"));
+
+        //Irá dar errado, ultrapassou o saldo
+        System.out.println((c2.transferir(c1, 40.00) ? "Operação realizada com sucesso!" : "ERRO!"));
+
+        //Irá dar errado, ultrapassou ambos limitantes
+        System.out.println((c2.transferir(c1, 320.00) ? "Operação realizada com sucesso!" : "ERRO!"));
+
+        //Irá dar certo, não ultrapassou o limite nem saldo
+        System.out.println((c1.transferir(c2, 50.00) ? "Operação realizada com sucesso!" : "ERRO!"));
+
+        //Irá dar errado, ultrapassou o saldo
+        System.out.println((c1.transferir(c2, 50.00) ? "Operação realizada com sucesso!" : "ERRO!"));
+
+        //Irá dar errado, ultrapassou o limite 
+        System.out.println((c1.transferir(c2, 100.00) ? "Operação realizada com sucesso!" : "ERRO!")); 
+
     }
   }
